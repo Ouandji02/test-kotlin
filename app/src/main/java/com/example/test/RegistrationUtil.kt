16e3382f645841407acea5e1e08db70dfd8aaf84
Ouandji02 <ouandjithierry00@gmail.com>
2023-02-16 17:51:12 +0100

    init test

---
+++ app/src/main/java/com/example/test/RegistrationUtil.kt
@@ -0,0 +1,26 @@
+package com.example.test
+
+object RegistrationUtil {
+
+    private val existingUsers = listOf("Peter","Carl")
+
+    /**
+     * the input is not valid  if...
+     * ...the username /password is empty
+     * ...the username is already taken
+     * ...the confirmed password is not the saame as the real password
+     */
+    fun validateRegistration(
+        username : String,
+        password : String,
+        confirmedPassword : String
+    ) : Boolean {
+
+        if (username.isEmpty() || password.isEmpty()) return  false
+        if (existingUsers.contains(username)) return false
+        if (confirmedPassword != password) return false
+        return true
+    }
+    
+
+}
---
+++ app/src/test/java/com/example/test/RegistrationUtilTest.kt
@@ -0,0 +1,56 @@
+package com.example.test
+
+import org.junit.Assert
+import org.junit.Test
+
+class RegistrationUtilTest {
+    @Test
+    fun `empty username returns false`() {
+        val result = RegistrationUtil.validateRegistration(
+            "",
+            "123",
+            "123"
+        )
+    }
+
+    @Test
+    fun `valid username and corectly repeated password returns true`(){
+        val result = RegistrationUtil.validateRegistration(
+            "Thierry",
+            "123",
+            "123"
+        )
+        Assert.assertTrue(result)
+    }
+
+    @Test
+    fun `username already exist returns false`(){
+        val result = RegistrationUtil.validateRegistration(
+            "Carl",
+            "123",
+            "123"
+        )
+        Assert.assertFalse(result)
+    }
+
+    @Test
+    fun `empty password returns false`(){
+        val result = RegistrationUtil.validateRegistration(
+            "Thierry",
+            "",
+            ""
+        )
+        Assert.assertFalse(result)
+    }
+
+    @Test
+    fun `confirmation password correct returns false`(){
+        val result = RegistrationUtil.validateRegistration(
+            "Thierry",
+            "123",
+            "124"
+        )
+        Assert.assertFalse(result)
+    }
+
+}
